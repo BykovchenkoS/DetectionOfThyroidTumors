f6249192af6986e0102268d138e4883492ec284a
Sonya Bykovchenko <sony.bykovchenko@gmail.com>
2025-02-08 19:27:34 +0300

    launching DETR

--- coco_check.py
+++ coco_check.py
@@ -7,69 +7,24 @@ from skimage import measure
 data = {
     "images": [
         {
-            "id": 100,
-            "file_name": "screen foto/dataset 2024-04-21 14_33_36/images_neuro_1/train/100.jpg",
-            "width": 375,
-            "height": 500
+            "id": 6,
+            "file_name": "dataset_coco_neuro_2/images_neuro_2/6.jpg",
+            "width": 171,
+            "height": 91
         }
     ],
     "annotations": [
         {
             "id": 0,
-            "image_id": 100,
+            "image_id": 6,
             "category_id": 0,
-            "segmentation": [
-                [
-                    0,
-                    180
-                ],
-                [
-                    0,
-                    416
-                ],
-                [
-                    357,
-                    406
-                ],
-                [
-                    364,
-                    175
-                ]
-            ],
-            "area": 84150,
+            "segmentation": "dataset_coco_neuro_2/masks/6_Node_1557921606.png",
+            "area": 198,
             "bbox": [
-                0,
-                175,
-                364,
-                241
-            ],
-            "iscrowd": 0
-        },
-        {
-            "id": 1,
-            "image_id": 100,
-            "category_id": 1,
-            "segmentation": "screen foto/dataset 2024-04-21 14_33_36/masks/100_Thyroid_tissue_1554990403.png",
-            "area": 577,
-            "bbox": [
-                155,
-                254,
-                92,
-                76
-            ],
-            "iscrowd": 0
-        },
-        {
-            "id": 2,
-            "image_id": 100,
-            "category_id": 2,
-            "segmentation": "screen foto/dataset 2024-04-21 14_33_36/masks/100_Carotis_1554990481.png",
-            "area": 583,
-            "bbox": [
-                243,
-                265,
-                34,
-                41
+                98.80701754385964,
+                173.4035087719298,
+                296.42105263157896,
+                166.73684210526315
             ],
             "iscrowd": 0
         }
@@ -77,21 +32,12 @@ data = {
     "categories": [
         {
             "id": 0,
-            "name": "sagital_longitudinal",
-            "supercategory": "organ"
-        },
-        {
-            "id": 1,
-            "name": "Thyroid tissue",
-            "supercategory": "organ"
-        },
-        {
-            "id": 2,
-            "name": "Carotis",
+            "name": "Node",
             "supercategory": "organ"
         }
     ]
 }
+
 
 category_colors = {
     0: (0.8, 0.2, 0.2),  # sagital_longitudinal - красный
@@ -109,13 +55,11 @@ for ann in data["annotations"]:
     color = category_colors.get(category_id, (1, 1, 1))
 
     if isinstance(ann["segmentation"], list):
-        # Если аннотация использует сегментацию в виде полигонов
         polygon = ann["segmentation"]
         poly = np.array(polygon)
         ax.fill(poly[:, 0], poly[:, 1], alpha=0.3, color=color)
 
     if isinstance(ann["segmentation"], str):
-        # Если аннотация использует битмап
         mask_path = ann["segmentation"]
         mask = Image.open(mask_path).convert("L")
         mask = np.array(mask)
@@ -124,8 +68,8 @@ for ann in data["annotations"]:
         contours = measure.find_contours(mask, 0.5)
         x, y, w, h = ann["bbox"]
         for contour in contours:
-            contour[:, 0] += y
-            contour[:, 1] += x
+            # contour[:, 0] += y
+            # contour[:, 1] += x
             ax.plot(contour[:, 1], contour[:, 0], color=color, linewidth=2)
 
     x, y, w, h = ann["bbox"]
